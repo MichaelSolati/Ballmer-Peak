0c1bd8ef37d10fe9d36dc12d8f802d5c83420f4c
MehdiSolati <mkslt04@gmail.com>
2015-10-16 09:31:44 -0700

    Initial commit with Materialize front-end framework.

---
+++ client/templates/navbar/navbar.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav({
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
