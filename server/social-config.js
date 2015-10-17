a2a37ac8ce00f33d5efdd160f37e3358fb64004e
MehdiSolati <mkslt04@gmail.com>
2015-10-16 20:52:15 -0700

    Attempt to fix redirect issue on Android Chrome browser during login. Redirect issue stems from Android Chrome version 46.0.2490.76

--- server/social-config.js
+++ server/social-config.js
@@ -13,21 +13,24 @@ Meteor.startup(function() {
   });
   // Add Facebook configuration entry
   ServiceConfiguration.configurations.insert({
-    "service": "facebook",
-    "appId": "880217695367546",
-    "secret": "203ca21587b7996aea9335ac56089d88"
+    service: "facebook",
+    appId: "880217695367546",
+    secret: "203ca21587b7996aea9335ac56089d88",
+    loginStyle: "popup"
   });
   // Add Google configuration entry
   ServiceConfiguration.configurations.insert({
-    "service": "google",
-    "clientId": "1054463848701-92aieqbtaflhl20o4tlaiaquaqsh8mi7.apps.googleusercontent.com",
-    "secret": "r5JQOwHSBBGoZvK6DEs94PIU"
+    service: "google",
+    clientId: "1054463848701-92aieqbtaflhl20o4tlaiaquaqsh8mi7.apps.googleusercontent.com",
+    secret: "r5JQOwHSBBGoZvK6DEs94PIU",
+    loginStyle: "popup"
   });
   // Add Twitter configuration entry
   ServiceConfiguration.configurations.insert({
-    "service": "twitter",
-    "consumerKey": "vWS7uLTWZiSB0IHfi25d7dMqP",
-    "secret": "OyfOy9vZoe5UglGD3HIfzLgfH1sWSrBMaXaGEUIfUX5c92ECkd"
+    service: "twitter",
+    consumerKey: "vWS7uLTWZiSB0IHfi25d7dMqP",
+    secret: "OyfOy9vZoe5UglGD3HIfzLgfH1sWSrBMaXaGEUIfUX5c92ECkd",
+    loginStyle: "popup"
   });
 });
 // Handle merge of application specific DB items
